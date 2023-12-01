fd1d2d1f9362cacb98d542e3397102a79afed684
saiganesh-marati <108809951+saiganesh-marati@users.noreply.github.com>
2023-12-01 19:13:29 +0530

    Update game.js

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ const gameBoard = document.getElementById('game-board')
 function main(currentTime) {
   if (gameOver) {
     if (confirm('You lost. Press ok to restart.')) { //okay-true
-      window.location = '/'
+      window.location = '/Snake-Game'
     }
     return
   }
